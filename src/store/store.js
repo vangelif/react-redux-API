7082f77f8310efb9844a797ac707501c70f975c6
Vangelis <ev.fotiades@yahoo.com>
2023-11-24 16:48:13 +0800

    build step-by-step breakdown in the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
+// STEP ONE: import configureStore from redux toolkit
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/usersSlice";
 
+// STEP TWO: build the store with the reducer,
+// and initial state for handling the users' actions
 const store = configureStore({
   reducer: {
     users: usersReducer,
